7d378e1945e723700b77abd1992893c5f5db9b13
malDev09 <159049731+malDev09@users.noreply.github.com>
2024-03-08 23:56:49 +0300

    feat: add basic structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AlbumsModule } from './app/albums/albums.module';
+import { ArtistsModule } from './app/artists/artists.module';
+import { FavoritesModule } from './app/favorites/favorites.module';
+import { TracksModule } from './app/tracks/tracks.module';
+import { UsersModule } from './app/users/users.module';
+import { InMemoryDBProvider } from 'libs/db/db.provider';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UsersModule,
+    AlbumsModule,
+    ArtistsModule,
+    FavoritesModule,
+    TracksModule,
+  ],
+  controllers: [],
+  providers: [InMemoryDBProvider],
 })
 export class AppModule {}
