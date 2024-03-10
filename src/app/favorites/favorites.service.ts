9d79c8051ab8ae156bd927384a3de294d5f0ab5a
malDev09 <159049731+malDev09@users.noreply.github.com>
2024-03-10 16:18:22 +0300

    fix: fix error types

--- src/app/favorites/favorites.service.ts
+++ src/app/favorites/favorites.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { ErrorCodes } from 'libs/error-codes.enum';
 import { Album, Artist, Favorites, Track } from 'libs/types/types';
 import { AlbumsRepo } from 'src/domain/repos/albums.repo';
 import { ArtistsRepo } from 'src/domain/repos/artists.repo';
@@ -26,21 +27,21 @@ export class FavoritesService {
       case 'artist':
         const artist = await this.artistsRepo.findOneById(id);
         if (!artist) {
-          throw new UnprocessableEntityException('Artist not found');
+          throw new UnprocessableEntityException(ErrorCodes.ARTIST_NOT_FOUND);
         }
         await this.favoritesRepo.addFavoriteArtist(artist);
         break;
       case 'album':
         const album = await this.albumsRepo.findOneById(id);
         if (!album) {
-          throw new UnprocessableEntityException('Album not found');
+          throw new UnprocessableEntityException(ErrorCodes.ALBUM_NOT_FOUND);
         }
         await this.favoritesRepo.addFavoriteAlbum(album);
         break;
       case 'track':
         const track = await this.tracksRepo.findOneById(id);
         if (!track) {
-          throw new UnprocessableEntityException('Track not found');
+          throw new UnprocessableEntityException(ErrorCodes.TRACK_NOT_FOUND);
         }
         await this.favoritesRepo.addFavoriteTrack(track);
         break;
